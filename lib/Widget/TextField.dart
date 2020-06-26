f0ebbad8b2e988866c6872262749ba526d8ea289
doganoguz <info@doganoguz.com>
2020-06-26 20:38:25 +0300

    Textfield, Button widget addet

--- lib/Widget/TextField.dart
+++ lib/Widget/TextField.dart
@@ -1,3 +1,35 @@
-/*
-Coming Soon..
-*/
+import 'package:flutter/material.dart';
+
+class TextFormField extends StatelessWidget {
+  final String hintText;
+  final Function validator;
+  final Function onSaved;
+  final bool isPassword;
+  final bool isEmail;
+  TextFormField({
+    this.hintText,
+    this.validator,
+    this.onSaved,
+    this.isPassword = false,
+    this.isEmail = false, InputDecoration decoration, bool obscureText, TextInputType keyboardType,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: hintText,
+          contentPadding: EdgeInsets.all(15.0),
+          border: InputBorder.none,
+          filled: true,
+          fillColor: Colors.grey[200],
+        ),
+        obscureText: isPassword ? true : false,
+        validator: validator,
+        onSaved: onSaved,
+        keyboardType: isEmail ? TextInputType.emailAddress : TextInputType.text,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:http/http.dart' as http;
 
 
 
-void main() {
+,void main() {
   runApp(CalisanlarApp());
 }
 
