32f3d0c3dafea9b1be982d2f407d041e29d444c7
doganoguz <info@doganoguz.com>
2020-07-02 18:02:12 +0300

    Form Password textfield added

--- lib/form.dart
+++ lib/form.dart
@@ -10,6 +10,7 @@ TextEditingController isim = new TextEditingController();
 TextEditingController soyisim = new TextEditingController();
 TextEditingController yas = new TextEditingController();
 TextEditingController aciklama = new TextEditingController();
+TextEditingController parola = new TextEditingController();
 
 class FormPage extends StatefulWidget {
 
@@ -28,6 +29,16 @@ class _FormPageState extends State<FormPage> {
   final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
 
+ bool _goster = true;
+
+String password;
+
+  void _toggle() {
+    setState(() {
+      _goster = !_goster;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -35,14 +46,17 @@ class _FormPageState extends State<FormPage> {
 
       home: Scaffold(
         appBar: AppBar(
+      
           backgroundColor: Color(0xff4875B0),
 
           title: Text("Form"),
           actions: <Widget>[
 
 
+      
           ],
       
+
         ),
 
 
@@ -58,7 +72,6 @@ class _FormPageState extends State<FormPage> {
       
                }
           
-
           },
           
           ),
@@ -146,9 +159,59 @@ class _FormPageState extends State<FormPage> {
                       border: InputBorder.none,
                       filled: true,
                       fillColor: Colors.grey[200],
+                     
                     ),
                   ),
                 ),
+                   Container(
+                  alignment: Alignment.topLeft,
+                  child:  Text(
+                    "Şifre:",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontWeight:FontWeight.w400,
+                      fontFamily: 'Poppins-Regular',
+
+                      height: 1.8,
+                    ),
+                  ),
+                ),
+ Container(
+                padding: EdgeInsets.all(8),
+                alignment: Alignment.centerLeft,
+                child: TextFormField(
+                  controller: parola,
+                  validator: (value) =>
+                  value.isEmpty ? 'Parola Boş Geçilemez!' : null,
+                  obscureText: _goster,
+                  keyboardType: TextInputType.visiblePassword,
+                  onSaved: (value) {
+                    password = value;
+                  },
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Poppins-Regular',
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(15.0),
+                    border: InputBorder.none,
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    suffixIcon: IconButton(
+                        icon: Icon(
+                          _goster
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+
+                        ),
+                        onPressed: () {
+                          _toggle();
+                        }),
+                  ),
+                ),
+              ),
                 Container(
                   alignment: Alignment.topLeft,
                   child:  Text(
