4848ef2f146314ee318fce8f09537c69e503c8ed
doganoguz <info@doganoguz.com>
2020-07-07 20:50:15 +0300

    Doğum Yeri Dropdown Eklendi

--- lib/form.dart
+++ lib/form.dart
@@ -5,7 +5,6 @@ Dogan OGUZ
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 TextEditingController isim = new TextEditingController();
 TextEditingController soyisim = new TextEditingController();
 TextEditingController yas = new TextEditingController();
@@ -13,25 +12,17 @@ TextEditingController aciklama = new TextEditingController();
 TextEditingController parola = new TextEditingController();
 
 class FormPage extends StatefulWidget {
-
-
   @override
   _FormPageState createState() => _FormPageState();
-
-
-
 }
 
 class _FormPageState extends State<FormPage> {
-
-
-
   final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
 
- bool _goster = true;
+  bool _goster = true;
 
-String password;
+  String password;
 
   void _toggle() {
     setState(() {
@@ -39,207 +30,159 @@ String password;
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       home: Scaffold(
         appBar: AppBar(
-      
           backgroundColor: Color(0xff4875B0),
-
           title: Text("Form"),
-          actions: <Widget>[
-
-
-      
-          ],
-      
-
+          actions: <Widget>[],
         ),
-
-
         floatingActionButton: FloatingActionButton(
           backgroundColor: Color(0xff4875B0),
           onPressed: () async {
-
-         final FormState form = _formKey.currentState;
-             if (form.validate()) {
-      
-              }
-             else {
-      
-               }
-          
+            final FormState form = _formKey.currentState;
+            if (form.validate()) {
+            } else {}
           },
-          
-          ),
-
-        body:SingleChildScrollView(
-
+        ),
+        body: SingleChildScrollView(
           padding: EdgeInsets.all(13),
-
-         
-
           child: Form(
-             key: _formKey,
+            key: _formKey,
             child: Column(
-
               children: <Widget>[
-
-
-
                 Container(
                   alignment: Alignment.topLeft,
-                  child:  Text(
+                  child: Text(
                     "İsim:",
                     textAlign: TextAlign.left,
                     style: TextStyle(
                       color: Colors.blueGrey,
-                      fontWeight:FontWeight.w400,
+                      fontWeight: FontWeight.w400,
                       fontFamily: 'Poppins-Regular',
-
                       height: 1.8,
                     ),
                   ),
                 ),
-
-
                 Container(
                   padding: EdgeInsets.all(8),
                   alignment: Alignment.centerLeft,
-                  child:  TextFormField(
-               
+                  child: TextFormField(
                     style: TextStyle(
-                      fontWeight:FontWeight.w500,
+                      fontWeight: FontWeight.w500,
                       fontFamily: 'Poppins-Regular',
                       color: Colors.black,
                     ),
                     decoration: InputDecoration(
-
                       contentPadding: EdgeInsets.all(15.0),
                       border: InputBorder.none,
                       filled: true,
                       fillColor: Colors.grey[200],
                     ),
-                   validator: (value) =>
-                  value.isEmpty ? 'İsim Alanı Boş Geçilemez!' : null,
+                    validator: (value) =>
+                        value.isEmpty ? 'İsim Alanı Boş Geçilemez!' : null,
                   ),
                 ),
                 Container(
                   alignment: Alignment.topLeft,
-                  child:  Text(
+                  child: Text(
                     "Soyisim:",
                     textAlign: TextAlign.left,
                     style: TextStyle(
                       color: Colors.blueGrey,
-                      fontWeight:FontWeight.w400,
+                      fontWeight: FontWeight.w400,
                       fontFamily: 'Poppins-Regular',
-
                       height: 1.8,
                     ),
                   ),
                 ),
-
-
                 Container(
                   padding: EdgeInsets.all(8),
                   alignment: Alignment.centerLeft,
-                  child:  TextFormField(
-                 
+                  child: TextFormField(
                     style: TextStyle(
-                      fontWeight:FontWeight.w500,
+                      fontWeight: FontWeight.w500,
                       fontFamily: 'Poppins-Regular',
                       color: Colors.black,
                     ),
                     decoration: InputDecoration(
-
                       contentPadding: EdgeInsets.all(15.0),
                       border: InputBorder.none,
                       filled: true,
                       fillColor: Colors.grey[200],
-                     
                     ),
                   ),
                 ),
-                   Container(
+                Container(
                   alignment: Alignment.topLeft,
-                  child:  Text(
+                  child: Text(
                     "Şifre:",
                     textAlign: TextAlign.left,
                     style: TextStyle(
                       color: Colors.blueGrey,
-                      fontWeight:FontWeight.w400,
+                      fontWeight: FontWeight.w400,
                       fontFamily: 'Poppins-Regular',
-
                       height: 1.8,
                     ),
                   ),
                 ),
- Container(
-                padding: EdgeInsets.all(8),
-                alignment: Alignment.centerLeft,
-                child: TextFormField(
-                  controller: parola,
-                  validator: (value) =>
-                  value.isEmpty ? 'Parola Boş Geçilemez!' : null,
-                  obscureText: _goster,
-                  keyboardType: TextInputType.visiblePassword,
-                  onSaved: (value) {
-                    password = value;
-                  },
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontFamily: 'Poppins-Regular',
-                    color: Colors.black,
-                  ),
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(15.0),
-                    border: InputBorder.none,
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    suffixIcon: IconButton(
-                        icon: Icon(
-                          _goster
-                        ? Icons.visibility
-                        : Icons.visibility_off,
-
-                        ),
-                        onPressed: () {
-                          _toggle();
-                        }),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.centerLeft,
+                  child: TextFormField(
+                    controller: parola,
+                    validator: (value) =>
+                        value.isEmpty ? 'Parola Boş Geçilemez!' : null,
+                    obscureText: _goster,
+                    keyboardType: TextInputType.visiblePassword,
+                    onSaved: (value) {
+                      password = value;
+                    },
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Poppins-Regular',
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(15.0),
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      suffixIcon: IconButton(
+                          icon: Icon(
+                            _goster ? Icons.visibility : Icons.visibility_off,
+                          ),
+                          onPressed: () {
+                            _toggle();
+                          }),
+                    ),
                   ),
                 ),
-              ),
                 Container(
                   alignment: Alignment.topLeft,
-                  child:  Text(
+                  child: Text(
                     "Yaş:",
                     textAlign: TextAlign.left,
                     style: TextStyle(
                       color: Colors.blueGrey,
-                      fontWeight:FontWeight.w400,
+                      fontWeight: FontWeight.w400,
                       fontFamily: 'Poppins-Regular',
-
                       height: 1.8,
                     ),
                   ),
                 ),
-
-
                 Container(
                   padding: EdgeInsets.all(8),
                   alignment: Alignment.centerLeft,
-                  child:  TextFormField(
-                    
+                  child: TextFormField(
                     style: TextStyle(
-                      fontWeight:FontWeight.w500,
+                      fontWeight: FontWeight.w500,
                       fontFamily: 'Poppins-Regular',
                       color: Colors.black,
                     ),
                     decoration: InputDecoration(
-
                       contentPadding: EdgeInsets.all(15.0),
                       border: InputBorder.none,
                       filled: true,
@@ -247,35 +190,29 @@ String password;
                     ),
                   ),
                 ),
-
                 Container(
                   alignment: Alignment.topLeft,
-                  child:  Text(
+                  child: Text(
                     "Açıklama:",
                     textAlign: TextAlign.left,
                     style: TextStyle(
                       color: Colors.blueGrey,
-                      fontWeight:FontWeight.w400,
+                      fontWeight: FontWeight.w400,
                       fontFamily: 'Poppins-Regular',
-
                       height: 1.8,
                     ),
                   ),
                 ),
-
-
                 Container(
                   padding: EdgeInsets.all(8),
                   alignment: Alignment.centerLeft,
-                  child:  TextFormField(
-                   
+                  child: TextFormField(
                     style: TextStyle(
-                      fontWeight:FontWeight.w500,
+                      fontWeight: FontWeight.w500,
                       fontFamily: 'Poppins-Regular',
                       color: Colors.black,
                     ),
                     decoration: InputDecoration(
-
                       contentPadding: EdgeInsets.all(15.0),
                       border: InputBorder.none,
                       filled: true,
@@ -283,17 +220,52 @@ String password;
                     ),
                   ),
                 ),
-
-
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Doğum Yeri :",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'Poppins-Regular',
+                      height: 1.8,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButtonFormField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(15.0),
+                        border: InputBorder.none,
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                      ),
+                      hint: Text("Ülke"),
+                      value: _selectednationality,
+                      onChanged: (newValue) {
+                        setState(() {
+                          //  _selectednationality = newValue;
+                          FocusScope.of(context).requestFocus(new FocusNode());
+                        });
+                      },
+                      items: _nationalityList.map((location) {
+                        return DropdownMenuItem(
+                          child: new Text(location),
+                          value: location,
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
         ),
-
       ),
-      debugShowCheckedModeBanner: false, 
-      
-       );
+    );
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,38 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_http_app/form.dart';
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' as http;  
-
-
-void main() {
-  runApp(CalisanlarApp());
-}
+import 'package:http/http.dart' as http;
+import 'package:flutter/cupertino.dart';
 
 class CalisanlarApp extends StatefulWidget {
   CalisanlarAppState createState() => CalisanlarAppState();
 }
 
 class CalisanlarAppState extends State<CalisanlarApp> {
-
-    @override
+  @override
   void initState() {
-   
     super.initState();
     this.veriGetir();
- 
   }
 
-  final String url = 'http://dummy.restapiexample.com/api/v1/employees'; 
-  
-  List data;  
+  final String url = 'http://dummy.restapiexample.com/api/v1/employees';
 
+  List data;
 
-  Future<String> veriGetir() async {                        
-    
-    var res = await http.get(Uri.encodeFull(url), 
-    headers: { 
-      'accept':'application/json'
-       });
+  Future<String> veriGetir() async {
+    var res = await http
+        .get(Uri.encodeFull(url), headers: {'accept': 'application/json'});
     setState(() {
       var content = json.decode(res.body);
       data = content['data'];
@@ -44,127 +33,110 @@ class CalisanlarAppState extends State<CalisanlarApp> {
 
   Widget build(context) {
     return MaterialApp(
-      
-        theme: ThemeData(
-    
+      theme: ThemeData(
         accentColor: Colors.amber,
-        
       ),
 
       debugShowCheckedModeBanner: false, //Debug bandını kaldırır
       title: 'Çalışan Listemiz',
       home: Scaffold(
+          appBar: AppBar(
+            title: Text('Çalışan Listemiz'),
+            backgroundColor: Colors.blueGrey,
+            actions: <Widget>[
+              new Stack(
+                children: <Widget>[
+                  new IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => FormPage()),
+                        );
 
-        appBar: AppBar(
-
-          title: Text('Çalışan Listemiz'),
-          backgroundColor: Colors.blueGrey,
-         
-          actions: <Widget>[
-            new Stack(
-              children: <Widget>[
-                new IconButton(
-                    icon: Icon(Icons.add),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => FormPage()));
-                    }),
-              ],
-            ),
-          ],
-
-        ),
-
-            body: Container(
-
-          margin: EdgeInsets.all(10.0),
-          child: ListView.builder(
-
-            itemCount: data == null ? 0:data.length,
-            itemBuilder: (BuildContext context, int index) {
-            
-              return Container(
-                child: Card(
+                        Navigator.pop(context);
+                      }),
+                ],
+              ),
+            ],
+          ),
+          body: Container(
+            margin: EdgeInsets.all(10.0),
+            child: ListView.builder(
+              itemCount: data == null ? 0 : data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                    child: Card(
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
-                    
                     children: <Widget>[
-                    ListTile(
-
-                      leading: Text(data[index]['id'], 
-                      style: TextStyle(fontSize: 30.0),
-
-                      ),
-                      
-                      title: Text(data[index]['employee_name'], 
-                      style: TextStyle(fontSize: 25.0, color: Colors.deepOrange, 
-                      fontWeight: FontWeight.bold),
-                      ),
-                    
-                      subtitle: Column(children: <Widget>[
-                        
-                        SizedBox(
-                          
-                          height: 10,
-                        
+                      ListTile(
+                        leading: Text(
+                          data[index]['id'],
+                          style: TextStyle(fontSize: 30.0),
                         ),
-                        Row(
+                        title: Text(
+                          data[index]['employee_name'],
+                          style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.deepOrange,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Column(
                           children: <Widget>[
-                            Text('ID : ', 
-                            style: TextStyle(fontWeight: FontWeight.bold),
+                            SizedBox(
+                              height: 10,
                             ),
-                            Text(data[index]['id'], 
-                            style: TextStyle(fontStyle: FontStyle.italic, 
-                            fontSize: 15.0),
+                            Row(
+                              children: <Widget>[
+                                Text(
+                                  'ID : ',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  data[index]['id'],
+                                  style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      fontSize: 15.0),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Text(
+                                  'Maaş : ',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(data[index]['employee_salary'])
+                              ],
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Text(
+                                  'Yaş : ',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(data[index]['employee_age'])
+                              ],
+                            ),
+                            SizedBox(
+                              height: 5,
                             ),
                           ],
                         ),
-                      SizedBox(
-                          
-                          height: 5,
-                        
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text('Maaş : ', 
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Text(data[index]['employee_salary'])
-                          ],
-                        ),
-                         SizedBox(
-                          
-                          height: 5,
-                        
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text('Yaş : ', style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Text(data[index]['employee_age'])
-                          ],
-                        ),
-                         SizedBox(
-                          
-                          height: 5,
-                        
-                        ),
-                      ],
                       ),
-                    ),
-                  
-                  ],),
-                )
-              );
-            },
-          ),
-        )
-    ),
-
-    
+                    ],
+                  ),
+                ));
+              },
+            ),
+          )),
     );
-    
   }
-
-
 }
