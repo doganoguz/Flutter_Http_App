16b4501f1cbaea54a94841cdf039282206455337
doganoguz <info@doganoguz.com>
2020-08-08 01:22:21 +0300

    service edit

--- lib/Utils/Service.dart
+++ lib/Utils/Service.dart
@@ -1,11 +1,6 @@
+
 import 'package:dio/dio.dart';
 
 class Service {
-  final client = Dio()..options.baseUrl = "http://192.168.1.1:3000";
-
-  Future<test> getService() async {
-    final response = await client.post('/test');
-    final data = response.data;
-    return test.fromJson(data);
-  }
-}
+  final client = Dio()
+    ..options.baseUrl = "https://192.168.1.1/user";
